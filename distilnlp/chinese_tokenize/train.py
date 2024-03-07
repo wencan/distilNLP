9a0481d38fc128bf5c8786d18c25fd4bfd6cacc6
wencan <wencan@live.cn>
2024-03-07 22:15:16 +0800

    tokenize add disable_lr_scheduler

--- distilnlp/chinese_tokenize/train.py
+++ distilnlp/chinese_tokenize/train.py
@@ -124,6 +124,7 @@ def cross_train_valid(accelerator: Accelerator,
                       num_epochs:int, 
                       checkpoint_interval:int,
                       num_workers: int,
+                      disable_lr_scheduler: bool = False,
                       lowest_lr:float = 0.000001,
                       ):
     valid_ratio = min(0.1, 100000/len(train_valid_set))
@@ -158,21 +159,22 @@ def cross_train_valid(accelerator: Accelerator,
             log(message)
             save_checkpoint(model, epoch, checkpoint, save_filedir, message)
 
-            accelerator.wait_for_everyone()
-            if lowest_train_loss is None or lowest_train_loss > train_loss:
-                # best state
-                lowest_train_loss = train_loss
-                best_model_state = model.state_dict()
-            if lowest_train_loss is not None and lowest_train_loss < train_loss:
-                # reset model
-                model.load_state_dict(best_model_state)
-                # reduce learning rate
-                learning_rate = learning_rate * 0.5
-                if learning_rate < lowest_lr:
-                    log(f'Finish early.')
-                    return
-                optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-                log(f'Update learning rate: {learning_rate}')
+            if not disable_lr_scheduler:
+                accelerator.wait_for_everyone()
+                if lowest_train_loss is None or lowest_train_loss > train_loss:
+                    # best state
+                    lowest_train_loss = train_loss
+                    best_model_state = model.state_dict()
+                if lowest_train_loss is not None and lowest_train_loss < train_loss:
+                    # reset model
+                    model.load_state_dict(best_model_state)
+                    # reduce learning rate
+                    learning_rate = learning_rate * 0.5
+                    if learning_rate < lowest_lr:
+                        log(f'Finish early.')
+                        return
+                    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+                    log(f'Update learning rate: {learning_rate}')
         
         # valid
         valid_loader = torch.utils.data.DataLoader(valid_set, batch_size, shuffle=True, collate_fn=collate_batch)
@@ -211,6 +213,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('--batch_size', type=int, default=1024, help='Number of samples per batch.')
     arg_parser.add_argument('--checkpoint_interval', type=int, default=0, help='Save a checkpoint every how many samples. Default is all train samples.')
     arg_parser.add_argument('--num_workers', type=int, default=0, help='The number of worker subprocesses. 0 indicates the use of only the main process.')
+    arg_parser.add_argument('--disable_lr_scheduler', type=bool, default=False, help='Disable learning rate scheduler.')
     args = arg_parser.parse_args()
 
     attention_implementation: Literal['local-attention', 'natten'] = args.attention_implementation
@@ -229,6 +232,7 @@ if __name__ == '__main__':
     batch_size = args.batch_size
     checkpoint_interval = args.checkpoint_interval
     num_workers = args.num_workers
+    disable_lr_scheduler = args.disable_lr_scheduler
 
     accelerator = Accelerator()
     device = accelerator.device
@@ -287,7 +291,7 @@ if __name__ == '__main__':
         test_acc, test_loss = valid(accelerator, model, test_loader)
         log(f'test accuracy: {test_acc:.6f} test loss: {test_loss:.6f}')
 
-    cross_train_valid(accelerator, model, collate_batch, train_valid_set, batch_size, learning_rate, num_epochs, checkpoint_interval, num_workers)
+    cross_train_valid(accelerator, model, collate_batch, train_valid_set, batch_size, learning_rate, num_epochs, checkpoint_interval, num_workers, disable_lr_scheduler)
 
     test_acc, test_loss = valid(accelerator, model, test_loader)
     log(f'test accuracy: {test_acc:.6f} test loss: {test_loss:.6f}')
